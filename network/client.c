bd52fd5eae0260ec4c2f1ae3a1484d2360021de2
Yu Nong <yu.nong@wsu.edu>
2020-04-19 20:47:31 -0700

    add packet sending and testing tool

---
+++ network/client.c
@@ -0,0 +1,92 @@
+#include <pcap.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/if_ether.h>
+#include <linux/if_packet.h>
+#include <netinet/ip.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+
+int main(int argc, char *argv[])
+{
+    if(argc!=4)
+    {
+        printf("./client server_ip, server_port TCP/UDP\n");
+        return 0;    
+    }
+    char ip[32];
+    strncpy(ip,argv[1],32);
+    int port;
+    sscanf(argv[2],"%d",&port);
+
+    char ebuf[PCAP_ERRBUF_SIZE];
+    pcap_t *p = pcap_open_offline("youtube.pcapng",ebuf);
+    struct pcap_pkthdr packet;
+    unsigned char sendStr[500][2000];
+    int len[500];
+    int flowLen=0;
+    for(flowLen=0;flowLen<500;flowLen++)
+    {
+        const unsigned char * pktStr = pcap_next(p,&packet);
+        if(pktStr == 0)
+            break;
+        memcpy(sendStr[flowLen],pktStr,packet.len);
+        len[flowLen]=packet.len;
+    }
+    pcap_close(p);
+    if(strcmp("TCP",argv[3])==0)
+    {
+        int s = socket(AF_INET, SOCK_STREAM, 0);
+        struct sockaddr_in addr;
+        memset(&addr,0,sizeof(addr));
+        addr.sin_family = AF_INET;
+        addr.sin_addr.s_addr = inet_addr(ip);
+        addr.sin_port = htons(port);
+        if(connect(s,(struct sockaddr*)&addr,sizeof(addr)))
+            printf("connect successfully\n");
+        //send(s,"123",5,0);
+    
+        while(1)
+        {
+            for(int i=0;i<flowLen;i++)
+            {   
+                struct iphdr *hdr;
+                hdr=sendStr[i];
+                sendto(s,sendStr[i]+54,len[i]-54,0,(struct sockaddr*)&addr,sizeof(addr));
+            }
+        /*printf("Packet length: %d\n", len[i]);
+        for(int j=0;j<len[i];j++)
+        {
+            printf("%x  ",sendStr[i][j]);
+        }
+        printf("\n\n\n\n");
+        */
+        }
+    }
+    else
+    {
+        int s = socket(AF_INET,SOCK_DGRAM,0);
+        struct sockaddr_in addr;
+        memset(&addr,0,sizeof(addr));
+        addr.sin_family = AF_INET;
+        addr.sin_addr.s_addr = inet_addr(ip);
+        addr.sin_port = htons(port);      
+        while(1)
+        {
+            for(int i=0;i<flowLen;i++)
+            {   
+                struct iphdr *hdr;
+                hdr=sendStr[i];
+                sendto(s,sendStr[i]+54,len[i]-54,0,(struct sockaddr*)&addr,sizeof(addr));
+            }
+
+        }
+             
+    }
+}
---
+++ network/server.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <time.h>
+int main(int argc, char *argv[])
+{
+    if(argc!=3)
+    {
+        printf("./server TCP/UDP port\n");
+        return 0;
+    }
+
+    int port;
+    sscanf(argv[2],"%d",&port);
+    int s;
+    
+    if(strcmp(argv[1],"TCP")==0)
+        s = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+    else if(strcmp(argv[1],"UDP")==0)
+        s = socket(AF_INET,SOCK_DGRAM,0);
+    else
+    {
+        printf("./server TCP/UDP port\n");
+        return 0;
+    }
+    struct sockaddr_in addr;
+    memset(&addr,0,sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(port);
+    bind(s,(struct sockaddr*)&addr,sizeof(addr));
+    if(strcmp(argv[1],"TCP")==0)
+    {
+        listen(s,20);
+        struct sockaddr_in client_addr;
+        socklen_t client_addr_size = sizeof(client_addr);
+        int client_sock = accept(s,(struct sockaddr*)&client_addr,&client_addr_size);
+        printf("connected %d\n",client_sock);
+        char buf[2000];
+        long pkt_recv=0;
+        time_t now,prev;
+        now=time(NULL);
+        prev=time(NULL);
+        while(1)
+        {
+            now=time(NULL);
+            if(now!=prev)
+            {
+                printf("Speed: %ld bps\n",pkt_recv*8);
+                pkt_recv=0;
+                prev=now;
+            }
+            pkt_recv+=recvfrom(client_sock,buf,2000,0,&client_addr,&client_addr_size);
+        }
+    }
+    else
+    {
+        char buf[2000];
+        long pkt_recv=0;
+        time_t now,prev;
+        now=time(NULL);
+        prev=time(NULL);
+        while(1)
+        {
+            now=time(NULL);
+            if(now!=prev)
+            {
+                printf("Speed: %ld bps\n",pkt_recv*8);
+                pkt_recv=0;
+                prev=now;
+            }
+            socklen_t sock_len = sizeof(addr);
+            pkt_recv+=recvfrom(s,buf,2000,0,&addr,&sock_len);
+        }
+
+    }
+}
